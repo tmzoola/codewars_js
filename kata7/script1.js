d7e8d806d729815f7e3427b6e45568f3a35ea345
Murodjon Tokhirov <murodtokh1997@gmail.com>
2023-04-09 15:43:19 +0900

    Kata 7 and initial commit

---
+++ kata7/script1.js
@@ -0,0 +1,21 @@
+// You are going to be given a word. 
+//Your job is to return the middle character of the word. 
+//If the word's length is odd, return the middle character. 
+//If the word's length is even, return the middle 2 characters.
+
+
+function getMiddle(s)
+{
+  if(s.length % 2 == 0){
+    a = s.length/2
+    return s.charAt(a-1) + s.charAt(a)
+  }
+  else{
+    a = s.length/2
+    b = parseInt(a)
+    return s.charAt(b)
+
+  }
+}
+
+console.log(getMiddle('testing'));
