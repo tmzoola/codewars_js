38e3f0566441ae9eb3745346ab8e79c142c30206
Murodjon Tokhirov <murodtokh1997@gmail.com>
2023-04-09 16:23:20 +0900

    new

--- kata7/script3.js
+++ kata7/script3.js
@@ -1,3 +1,10 @@
+//Return the number (count) of vowels in the given string.
+
+//We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+//The input string will only consist of lower case letters and/or spaces.
+
+
 function getCount(str) {
     str1 = ""
     for (let i = 0; i < str.length; i++) {
